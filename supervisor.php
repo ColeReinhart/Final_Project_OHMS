59ad3a02b449161e0d972ffcdf7ca098e2011cf9
Zach Lehman <zalehman182@stevenscollege.edu>
2019-11-14 08:45:22 -0500

    Fixed the login for family

--- supervisor.php
+++ supervisor.php
@@ -3,7 +3,7 @@ include_once 'db.php';
 
 session_start();
 
-if(($_SESSION['loggedIn'] = true) && ($_SESSION['role'] == "supervisor") || $_SESSION['role'] == "admin") {
+if(($_SESSION['loggedIn'] = true) && ($_SESSION['role'] == "Supervisor") || $_SESSION['role'] == "admin") {
     // echo $_SESSION['role'];
 } else {
     header("location: index.php");
