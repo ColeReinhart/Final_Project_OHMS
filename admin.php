3301605fcc4d366c9fa2c65e9fed696ad81d216a
Zach Lehman <zalehman182@stevenscollege.edu>
2019-11-14 08:05:44 -0500

    Add nav bar on Admin Page

--- admin.php
+++ admin.php
@@ -22,6 +22,43 @@ if(isset($_GET['logout'])) {
             <button type="submit" class="btn" name = "logout">Logout</button>
         </form>
         <h1>Admin Home</h1>
+
+        <ul>
+            <li><a class="active" href="">Additional Information</a></li>
+            <li><a href="">Roles</a></li>
+             <li><a href="">Employee</a></li>
+            <li><a href="">Patients</a></li>
+        </ul>
+
+        <label>Date:</label>
+        <input type="text">
+        <br>
         
+        <table>
+            <tr>
+                <th>Supervisor</th>
+                <th>Doctor</th>
+                <th>Caregiver1</th>
+                <th>Caregiver2</th>
+                <th>Caregiver3</th>
+                <th>Caregiver4</th>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td>Name</td>
+                <td>Name</td>
+                <td>Name</td>
+                <td>Name</td>
+                <td>Name</td>
+            </tr>
+            <tr>
+                <td>Group Name</td>
+                <td>Group Name</td>
+                <td>Group Name</td>
+                <td>Group Name</td>
+                <td>Group Name</td>
+                <td>Group Name</td>
+            </tr>
+        </table>
     </body>
 </html>
