f55c7a35208c2b54d3a79691888e836ce0f4af06
Zach Lehman <zalehman182@stevenscollege.edu>
2019-11-25 10:42:29 -0500

    Approval and Not Approval of the users on this application

--- reg_app.php
+++ reg_app.php
@@ -14,6 +14,42 @@ if(isset($_GET['logout'])) {
     header("location: index.php");
 }
 
+if(isset($_POST['empapprove'])) {
+    $empupdate = "UPDATE Employee SET Employee.Approved = 1
+    WHERE Employee.Emp_ID = {$_POST['empapprove']};";
+    mysqli_query($conn, $empupdate);;
+}
+
+if(isset($_POST['empblock'])) {
+    $empblock = "DELETE FROM Employee
+    WHERE Employee.Emp_ID = {$_POST['empblock']};";
+    mysqli_query($conn, $empblock);;
+}
+
+if(isset($_POST['famapprove'])) {
+    $famupdate = "UPDATE FAMILY_MEMBER SET FAMILY_MEMBER.Approved = 1
+    WHERE FAMILY_MEMBER.FAM_ID = {$_POST['famapprove']};";
+    mysqli_query($conn, $famupdate);;
+}
+
+if(isset($_POST['famblock'])) {
+    $famblock = "DELETE FROM FAMILY_MEMBER
+    WHERE FAMILY_MEMBER.FAM_ID = {$_POST['famblock']};";
+    mysqli_query($conn, $famblock);;
+}
+
+if(isset($_POST['patapprove'])) {
+    $patupdate = "UPDATE Patient SET Patient.Approved = 1
+    WHERE Patient.Pat_ID = {$_POST['patapprove']};";
+    mysqli_query($conn, $patupdate);;
+}
+
+if(isset($_POST['patblock'])) {
+    $patblock = "DELETE FROM Patient
+    WHERE Patient.Pat_ID = {$_POST['patblock']};";
+    mysqli_query($conn, $patblock);;
+}
+
 ?>
 <html>
 <head>
@@ -40,23 +76,83 @@ if(isset($_GET['logout'])) {
             <li><a href="ad_report.php">Admin's Report</a></li>
             <li><a href="payment.php">Payment</a></li>
         </ul>
-        
+
         <table>
             <tr>
-                <th>Name</th>
+                <th>First Name</th>
+                <th>Last Name</th>
                 <th>Role</th>
-                <th>Yes</th>
-                <th>No</th>
+                <th>Approve</th>
+                <th>Not Approve</th>
             </tr>
             <tr>
-                <td>Name</td>
-                <td>Name</td>
-                <td><input type="checkbox"></td>     <td><input type="checkbox"></td>
+                <?php
+                echo "<h3> Employees </h3>";
+                $emprole = "SELECT Employee.Fname, Employee.Lname, Employee.Role, Employee.Emp_ID FROM Employee WHERE Employee.Approved = 0;";
+                $empresult = mysqli_query($conn, $emprole);
+                if($empresult) {
+                    while($emprow = mysqli_fetch_row($empresult)) {
+                        echo "<th>$emprow[0]</th>";
+                        echo "<th>$emprow[1]</th>";
+                        echo "<th>$emprow[2]</th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'empapprove' type = 'submit' value = $emprow[3]></th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'empblock' type = 'submit' value = $emprow[3]></th>";
+                        echo "</tr>";
+                    }
+                }
+                echo "</table>";
+                echo "<h3> Family Members </h3>";
+                echo "<table>";
+                echo "<tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th>Approve</th>
+                <th>Not Approve</th>
+                </tr>";
+                $famrole = "SELECT FAMILY_MEMBER.Fname, FAMILY_MEMBER.Lname, FAMILY_MEMBER.Role, FAMILY_MEMBER.FAM_ID FROM FAMILY_MEMBER WHERE FAMILY_MEMBER.Approved = 0;";
+                $famresult = mysqli_query($conn, $famrole);
+                if($famresult) {
+                    while($famrow = mysqli_fetch_row($famresult)) {
+                        echo "<th>$famrow[0]</th>";
+                        echo "<th>$famrow[1]</th>";
+                        echo "<th>$famrow[2]</th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'famapprove' type = 'submit' value = $famrow[3]></th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'famblock' type = 'submit' value = $famrow[3]></th>";
+                        echo "</tr>";
+                    }
+                }
+                echo "</table>";
+                echo "<h3> Patients</h3>";
+                echo "<table>";
+                echo "<tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th>Approve</th>
+                <th>Not Approve</th>
+                </tr>";
+                $patrole = "SELECT Patient.Fname, Patient.Lname, Patient.Role, Patient.Pat_ID FROM Patient WHERE Patient.Approved = 0;";
+                $patresult = mysqli_query($conn, $patrole);
+                if($patresult) {
+                    while($patrow = mysqli_fetch_row($patresult)) {
+                        echo "<th>$patrow[0]</th>";
+                        echo "<th>$patrow[1]</th>";
+                        echo "<th>$patrow[2]</th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'patapprove' type = 'submit' value = $patrow[3]></th>";
+                        echo "<th><form method = 'POST' action = 'reg_app.php'>
+                        <input name = 'patblock' type = 'submit' value = $patrow[3]></th>";
+                        echo "</tr>";
+                    }
+                }
+            ?>
             </tr>
         </table>
-
-        <button>OKAY</button>
-        <button>CANCEL</button>
         
         <footer>
             <ul>
